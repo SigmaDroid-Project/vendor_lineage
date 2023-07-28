cd31b506bd9415679031015121bf184e7b0274d9
Matt Filetto <matt.filetto@gmail.com>
2023-07-28 01:18:52 -0700

    build: refactor updatepackage task for SigmaDroid
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- build/tasks/updatepackage.mk
+++ build/tasks/updatepackage.mk
@@ -18,15 +18,29 @@
 ECHO_BLUE := \e[34m
 ECHO_GREEN := \e[32m
 ECHO_ENDCOLOR := \e[0m
+SIGMA_TARGET_UPDATEPACKAGE := $(PRODUCT_OUT)/$(SIGMA_VERSION)_fastboot.zip
 
 .PHONY: updatepackage
 updatepackage: $(INTERNAL_UPDATE_PACKAGE_TARGET)
-#	$(hide) ln -f $(INTERNAL_UPDATE_PACKAGE_TARGET) $(SPARK_TARGET_UPDATEPACKAGE)
+	$(hide) mv -f $(INTERNAL_UPDATE_PACKAGE_TARGET) $(SIGMA_TARGET_UPDATEPACKAGE)
 	@echo "Generating changelog..."
 	$(hide) ./vendor/lineage/tools/changelog.sh
-	$(hide) cp Changelog.txt $(PRODUCT_OUT)/$(LINEAGE_VERSION)-Changelog.txt
-#	$(hide) ./vendor/lineage/build/tasks/betadroid_ascii.sh
+	$(hide) cp Changelog.txt $(PRODUCT_OUT)/$(SIGMA_VERSION)-Changelog.txt
+	$(hide) cp Changelog.txt $(PRODUCT_OUT)/Auto-Changelog.txt
+
+	@echo '' >&2
+	@echo '  _____  _                           _____                _      _  ' <&2
+	@echo ' / ____|(_)                         |  __ \              (_)    | | ' <&2
+	@echo '| (___   _   __ _  _ __ ___    __ _ | |  | | _ __   ___   _   __| | ' <&2
+	@echo ' \___ \ | | / _` || '_ ` _ \  / _` || |  | || '__| / _ \ | | / _` | ' <&2
+	@echo ' ____) || || (_| || | | | | || (_| || |__| || |   | (_) || || (_| | ' <&2
+	@echo '|_____/ |_| \__, ||_| |_| |_| \__,_||_____/ |_|    \___/ |_| \__,_| ' <&2
+	@echo '             __/ |                                                  ' <&2
+	@echo '            |___/                                                   ' <&2
+	@echo '' >&2
+	@echo '' >&2
 	@echo -e "$(ECHO_GREEN)==============================================================${ECHO_ENDCOLOR}"
-	@echo -e " ${ECHO_BLUE}Generated Changelog Successfully:${ECHO_ENDCOLOR}"
+	@echo -e "${ECHO_BLUE}Generated Changelog Successfully${ECHO_ENDCOLOR}"
+	@echo -e "${ECHO_BLUE}Package Complete: $(SIGMA_TARGET_UPDATEPACKAGE) ${ECHO_ENDCOLOR}"
 	@echo -e "${ECHO_GREEN}==============================================================${ECHO_ENDCOLOR}"
 	@echo ""
