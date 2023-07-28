c4db52dbe95b75ec5893e002e86bb939ed847800
Matt Filetto <matt.filetto@gmail.com>
2023-07-28 13:36:10 -0700

    tools: changelog generator: cleanup dead code
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -21,22 +21,6 @@ touch $Changelog
 # Print something to build output
 echo "Generating changelog..."
 
-    # # # Cycle through all available repos
-	#  	export After_Date=$(/usr/bin/date --date="14 days ago" +%Y/%m/%d)
-    #  REPO_LIST="$(repo list --path | sed 's|^vendor/OTA$||')"
-    #  for repo_path in $REPO_LIST; do
-    #      # Find commits from 13 days ago until now
-    #      GIT_LOG="$(git -C "$repo_path" log --since=$After_Date --pretty=format:"%cd - %s (by %an)" --date=format:"%Y-%m-%d")"
-    #      [ -n "$GIT_LOG" ] && {
-    #          printf '\n------------------------------------------  ';
-	# 		 printf '\n'
-	# 		 echo "$repo_path"
-	#  		 echo "------------------------------------------"
-    #          echo "$GIT_LOG"
-	# 		 printf '\n'
-    #      } >> $Changelog
-    #  done
-
 # define changelog_days using 'export changelog_days=10'
 # this can be done before intiate build environment (. build/envsetup.sh)
 if [ -z $changelog_days ];then
