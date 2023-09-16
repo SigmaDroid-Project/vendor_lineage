ffb4093bb5578b0391389fb5711401b4a1f600d8
Matt Filetto <matt.filetto@gmail.com>
2023-09-16 04:37:14 -0700

    config: default to gesture navigation instead of navbar.
    * Match Pixel defaults.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- config/sigma.mk
+++ config/sigma.mk
@@ -1,3 +1,7 @@
 # Packages
 PRODUCT_PACKAGES += \
     SigmaWallpapersStub
+
+# SystemUI Properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
