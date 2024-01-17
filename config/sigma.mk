7bcc2ada34760350df32c890c9ad36d8a7ee7c64
Matt Filetto <matt.filetto@gmail.com>
2024-01-17 01:29:11 -0800

    Build Pixel Face Unlock support if it exists & TARGET_DEVICE is a Pixel.
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- config/sigma.mk
+++ config/sigma.mk
@@ -1,1 +1,6 @@
 PRODUCT_BRAND ?= Sigma
+
+# Pixel Face Unlock
+ifneq ($(filter blueline crosshatch bonito sargo flame coral sunfish bramble barbet redfin oriole raven bluejay panther cheetah lynx husky shiba, $(TARGET_DEVICE)),)
+-include vendor/google/faceunlock/device.mk
+endif
