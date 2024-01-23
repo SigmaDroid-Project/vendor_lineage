bf0be3a7bcce96894ede727432f10f9c7d55cce3
Matt Filetto <matt.filetto@gmail.com>
2024-01-22 20:24:14 -0800

    add RRO to enable Clear Calling settings
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- config/sigma.mk
+++ config/sigma.mk
@@ -1,5 +1,9 @@
 PRODUCT_BRAND ?= Sigma
 
+# RRO Packages
+PRODUCT_PACKAGES += \
+    ClearCallingSettingsOverlay
+
 # Pixel Face Unlock
 ifneq ($(filter blueline crosshatch bonito sargo flame coral sunfish bramble barbet redfin oriole raven bluejay panther cheetah lynx husky shiba, $(TARGET_DEVICE)),)
 -include vendor/google/faceunlock/device.mk
