8c4a345c245a65e851bbc0b65bbb1ec42f3bd710
Matt Filetto <matt.filetto@gmail.com>
2024-03-20 07:00:36 -0700

    config: fix guard for some pixel features [SQUASH]
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    
    config: set some pixel specific default values.
    
    * RROs for SettingsGoogle & SystemUIGoogle to enable some features by default to match stock.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    
    Include Clear calling only for those devices that support it
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- config/sigma.mk
+++ config/sigma.mk
@@ -36,3 +36,16 @@ PRODUCT_PACKAGES += \
     AndroidOverlay \
     Launcher3Overlay \
     SettingsOverlay
+
+# Pixel Specific
+ifeq ($(TARGET_IS_PIXEL),true)
+PRODUCT_PACKAGES += \
+    PixelSettingsGoogleOverlay \
+    PixelSystemUIGoogleOverlay
+endif
+
+# Clear Calling (P22+)
+ifeq ($(TARGET_SUPPORTS_CLEAR_CALLING),true)
+PRODUCT_PACKAGES += \
+    ClearCallingSettingsOverlay
+endif
