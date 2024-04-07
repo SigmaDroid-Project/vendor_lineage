354947f7c81d1e03d9f6635bc0eca0a44b5f1dde
Matt Filetto <matt.filetto@gmail.com>
2024-04-07 00:44:37 -0700

    Add initial SigmaDroid properties
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

---
+++ config/sigma.mk
@@ -0,0 +1,24 @@
+PRODUCT_BRAND ?= Sigma
+
+# Certification (SafetyNet)
+$(call inherit-product-if-exists, vendor/certification/config.mk)
+
+ifeq ($(TARGET_INCLUDE_MATLOG),true)
+PRODUCT_PACKAGES += \
+    MatLog
+endif
+
+# Privapp-permissions whitelist mode
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.control_privapp_permissions=log
+
+# Disable blur on app launch
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=0
+
+# GMS
+ifeq ($(WITH_GMS),true)
+BUILD_GMS_OVERLAYS_AND_PROPS := true
+$(call inherit-product, vendor/gms/products/gms.mk)
+endif
+
