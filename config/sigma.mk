156610cdff5d737f0db8147231f4cb91e8fe4ff2
Matt Filetto <matt.filetto@gmail.com>
2024-08-30 07:01:07 -0700

    config: add PIF build properties
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- config/sigma.mk
+++ config/sigma.mk
@@ -45,3 +45,22 @@ endif
 # Launcher3 as Default Launcher
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.default_launcher=0
+
+# Pif
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_mainline_BRAND?=google \
+    persist.sys.pihooks_mainline_DEVICE?=caiman \
+    persist.sys.pihooks_mainline_MANUFACTURER?=Google \
+    persist.sys.pihooks_mainline_PRODUCT?=caiman \
+    ro.product.model_for_attestation?=Pixel 9 Pro \
+    ro.product.brand_for_attestation?=google \
+    ro.product.name_for_attestation?=caiman \
+    ro.product.device_for_attestation?=caiman \
+    ro.product.manufacturer_for_attestation?=Google
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.pihooks.first_api_level=32
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PIHOOKS_BUILD_FINGERPRINT="google/caiman/caiman:14/AD1A.240530.047.U1/12150698:user/release-keys" \
+    PIHOOKS_MODEL_SPOOF="Pixel 9 Pro"
