36096cb0134f4235370dd3f3f9f966e65ddbeb44
Matt Filetto <matt.filetto@gmail.com>
2024-09-11 15:21:43 -0700

    Add initial SigmaDroid properties
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -1,11 +1,28 @@
 PRODUCT_VERSION_MAJOR = 14
 PRODUCT_VERSION_MINOR = 0
 
-# Increase CR Version with each major release.
-CR_VERSION := 10.8
+SIGMA_VERSION := v4.9.0
+SIGMA_VARIANT := Droid
+
+SIGMA_BUILD_TYPE ?= UNOFFICIAL
+
+# Only include Updater for official builds
+ifeq ($(filter-out OFFICIAL Official official,$(SIGMA_BUILD_TYPE)),)
+PRODUCT_PACKAGES += \
+    SigmaUpdater
+
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
+endif
+
+ifeq ($(WITH_GMS),true)
+SIGMA_BUILD_PACKAGE := gapps
+else
+SIGMA_BUILD_PACKAGE := vanilla
+endif
 
 # Internal version
-LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)
+LINEAGE_VERSION := Sigma$(SIGMA_VARIANT)-$(SIGMA_VERSION)-$(shell date +%Y%m%d)-$(SIGMA_BUILD_TYPE)-$(SIGMA_BUILD_PACKAGE)-$(LINEAGE_BUILD)
 
 # Display version
-LINEAGE_DISPLAY_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-v$(CR_VERSION)
+LINEAGE_DISPLAY_VERSION := Sigma$(SIGMA_VARIANT)-$(SIGMA_VERSION)-$(SIGMA_BUILD_PACKAGE)-$(LINEAGE_BUILD)
