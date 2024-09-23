cb374669441d6355cd73227370e98ee1a07db038
Matt Filetto <matt.filetto@gmail.com>
2024-09-23 01:43:39 -0700

    rro: drop redundant packages
    * These are already set in vendor/pixel-style by the SettingsGoogleOverlay RRO
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- config/sigma.mk
+++ config/sigma.mk
@@ -20,19 +20,6 @@ PRODUCT_PACKAGES += \
     AndroidOverlay \
     Launcher3Overlay
 
-# Pixel Specific
-ifeq ($(TARGET_IS_PIXEL),true)
-PRODUCT_PACKAGES += \
-    PixelSettingsGoogleOverlay \
-    PixelSystemUIGoogleOverlay
-endif
-
-# Clear Calling (P22+)
-ifeq ($(TARGET_SUPPORTS_CLEAR_CALLING),true)
-PRODUCT_PACKAGES += \
-    ClearCallingSettingsOverlay
-endif
-
 # Launcher3 as Default Launcher
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.default_launcher=0 \
