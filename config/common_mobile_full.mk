fa0ffccce54d813c82b8d448d840bd5c816a52b2
Matt Filetto <matt.filetto@gmail.com>
2024-09-23 02:32:06 -0700

    config: stop excluding AudioFX from GMS builds
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -20,11 +20,9 @@ PRODUCT_PACKAGES += \
     Aperture
 endif
 
-ifeq ($(WITH_GMS),false)
 ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
 PRODUCT_PACKAGES += \
     AudioFX
-endif
 endif
 
 # Extra cmdline tools
